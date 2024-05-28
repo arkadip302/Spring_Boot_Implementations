8b68f1bb9e34574f040c96066429dab1c862f516
arkadip302 <1706302@kiit.ac.in>
2024-05-29 00:06:41 +0530

    Change

--- Normal_API_Call/apiCall/src/main/java/com/arkadip/apiCall/ApiCallApplication.java
+++ Normal_API_Call/apiCall/src/main/java/com/arkadip/apiCall/ApiCallApplication.java
@@ -2,10 +2,8 @@ package com.arkadip.apiCall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.arkadip.apiCall.service")
 public class ApiCallApplication {
 
 	public static void main(String[] args) {
